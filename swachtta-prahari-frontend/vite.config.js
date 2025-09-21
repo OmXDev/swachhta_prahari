cfeb88bdf40b915cffd66a4f9ecc13b6b3c1fe3e
OmXDev <omgupta.tech@gmail.com>
2025-09-22 03:13:52 +0530

    errorfixed.

--- swachtta-prahari-frontend/vite.config.js
+++ swachtta-prahari-frontend/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
   server: { port: 3000, open: true },
-  build: { outDir: 'build', sourcemap: true }
+  build: { outDir: 'dist', sourcemap: true }
 })
